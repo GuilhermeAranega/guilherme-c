654f3d14d0a03387add8beda23658a552945d8c3
Guilherme Amo Dauksys Aranega <guilhermee.aranega@gmail.com>
2023-03-02 21:40:42 -0300

    Create population.c

---
+++ population.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while (start < 9);
+    // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
+    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    while (start < end)
+    {
+        int x = start / 3;
+        int y = start / 4;
+        start = start + x - y;
+        years++;
+    }
+
+
+    // TODO: Print number of years
+    printf("Years: %d\n", years);
+}
